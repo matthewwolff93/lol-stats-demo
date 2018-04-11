0810b6ffa48011a9e122e51db6dee63cec44feb7
Matthew Wolff <matthew.wolff93@gmail.com>
2018-04-11 20:31:11 +1200

    Add loading animation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -43,6 +43,7 @@ class App extends Component {
 				</header>
 				<main>
 					{this.state.matches && <MatchList matches={this.state.matches} accountId={this.accountId} />}
+					{!this.state.matches && <div className="loader-container"><div className="loader"></div></div>}
 				</main>
 			</div>
 		);
