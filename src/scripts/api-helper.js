a461952992e6ccfeb965bc8b1e4f193e93d09150
Matthew Wolff <matthew.wolff93@gmail.com>
2018-04-11 20:18:16 +1200

    Use environment variable to hide api key

--- src/scripts/api-helper.js
+++ src/scripts/api-helper.js
@@ -4,7 +4,7 @@ export function fetchApiData(subUrl) {
 	// Need this proxy to prevent CORS issue with GET request
 	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 	// TODO: Hide api key
-	const apiKey = 'RGAPI-1b1f38f9-e123-4b9b-a02b-4d414167b8e3'
+	const apiKey = process.env.REACT_APP_API_KEY;
 	const url = `https://oc1.api.riotgames.com/lol/${subUrl}/?api_key=${apiKey}`;
 
 	return axios.get(proxyUrl + url);
